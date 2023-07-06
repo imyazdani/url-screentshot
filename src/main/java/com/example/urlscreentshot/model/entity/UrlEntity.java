7ad077f143aa4644457e0fddda3e01a84576e200
Mohsen Yazdani <imyazdani@gmail.com>
2023-07-06 20:20:34 +0330

    Config postgresql and entities

--- src/main/java/com/example/urlscreentshot/model/entity/UrlEntity.java
+++ src/main/java/com/example/urlscreentshot/model/entity/UrlEntity.java
@@ -1,14 +1,33 @@
 package com.example.urlscreentshot.model.entity;
 
 import com.example.urlscreentshot.model.enums.UrlStatus;
-import lombok.Builder;
+import jakarta.persistence.*;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.Instant;
 
 @Data
-@Builder
+@Entity
+@Table(name = "urls")
 public class UrlEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(unique = true)
     private String urlId;
+
+    @Column(nullable = false)
     private String url;
+
+    @Column(nullable = false)
     private UrlStatus status;
+
+    @CreationTimestamp
+    private Instant createdOn;
+
+    @UpdateTimestamp
+    private Instant lastUpdatedOn;
 }
