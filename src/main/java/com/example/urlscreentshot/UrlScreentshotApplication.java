bc33a8243d79124d4a074ead361ac20f629394ea
Mohsen Yazdani <imyazdani@gmail.com>
2023-07-06 11:07:51 +0330

    Initial commit

---
+++ src/main/java/com/example/urlscreentshot/UrlScreentshotApplication.java
@@ -0,0 +1,13 @@
+package com.example.urlscreentshot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlScreentshotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrlScreentshotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/urlscreentshot/UrlScreentshotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.urlscreentshot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlScreentshotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
